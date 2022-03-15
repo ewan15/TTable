7d7699540ff8fbc591b99671d701d4fad51adabe
Ewan Bains <ewan.bains@gmail.com>
2022-03-15 23:38:45 +0100

    FEAT: Setup

---
+++ tests/ttableTest.cpp
@@ -0,0 +1,4 @@
+//
+// Created by ewan on 3/15/22.
+//
+
---
+++ ttable/include/ttable/ttable.h
@@ -0,0 +1,15 @@
+//
+// Created by ewan on 3/15/22.
+//
+
+#ifndef TTABLE_TTABLE_H
+#define TTABLE_TTABLE_H
+
+namespace TTable {
+    namespace <auto T>
+    class Table {
+
+    };
+}
+
+#endif //TTABLE_TTABLE_H
