67c21c77801c84c8f8a4e80d8f4edc5a73168766
Ewan Bains <ewan.bains@gmail.com>
2022-04-03 16:08:08 +0200

    REFACTOR: Changing to microsoft formating

--- ttable/include/ttable/ttable.h
+++ ttable/include/ttable/ttable.h
@@ -9,37 +9,48 @@
 #include <algorithm>
 #include <cstdlib>
 
-namespace TTable {
-template <std::size_t N> struct StringLiteral {
-  constexpr StringLiteral(const char (&str)[N]) { std::copy_n(str, N, value); }
+namespace TTable
+{
+template <std::size_t N> struct StringLiteral
+{
+    constexpr StringLiteral(const char (&str)[N])
+    {
+        std::copy_n(str, N, value);
+    }
 
-  char value[N];
-  std::size_t len = N;
+    char value[N];
+    std::size_t len = N;
 };
 
-template <StringLiteral n, typename T> struct column {
-  static constexpr auto name = n;
-  static std::vector<T> vec;
+template <StringLiteral n, typename T> struct column
+{
+    static constexpr auto name = n;
+    static std::vector<T> vec;
 };
 
-template <auto c, auto t> struct Table {
-  static constexpr auto col = c;
-  static constexpr auto tail = t;
+template <auto c, auto t> struct Table
+{
+    static constexpr auto col = c;
+    static constexpr auto tail = t;
 };
 
-template <auto col> auto createTable() { return Table<col, nullptr>(); }
-
-template <auto col, auto col2, auto... cols> auto createTable() {
-  auto tail = createTable<col2, cols...>();
-  return Table<col, tail>();
+template <auto col> auto createTable()
+{
+    return Table<col, nullptr>();
 }
 
-template <auto table, StringLiteral name> auto getColumnByName() {
-  if constexpr (table.col.name.len == name.len &&
-                std::string_view(table.col.name.value) == name.value)
-    return table.col;
-  else
-    return getColumnByName<table.col, name>();
+template <auto col, auto col2, auto... cols> auto createTable()
+{
+    auto tail = createTable<col2, cols...>();
+    return Table<col, tail>();
+}
+
+template <auto table, StringLiteral name> auto getColumnByName()
+{
+    if constexpr (table.col.name.len == name.len && std::string_view(table.col.name.value) == name.value)
+        return table.col;
+    else
+        return getColumnByName<table.col, name>();
 }
 } // namespace TTable
 
