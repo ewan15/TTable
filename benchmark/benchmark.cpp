4fbf126815a86217244d2aff1dfc89650606a784
Ewan Bains <ewan.bains@gmail.com>
2022-04-16 14:18:19 +0200

    FEAT: Adding two column benchmark

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -31,4 +31,40 @@ static void BM_MultiPushBack(benchmark::State& state) {
 // Register the function as a benchmark
 BENCHMARK(BM_MultiPushBack);
 
+static void BM_AddColumn(benchmark::State& state) {
+    using col = TTable::Column<"id", int>;
+    using col2 = TTable::Column<"age", int>;
+    auto table = TTable::create_table<col, col2>();
+    for (auto i = 0; i < ELEMENTS_PUSH_BACK; i++)
+    {
+        TTable::push_back(table, 5, 4);
+    }
+
+    for (auto _ : state)
+    {
+        auto newTable = TTable::add_column<col2>(table);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_AddColumn);
+
+static void BM_AddTwoColumn(benchmark::State& state) {
+    using Col = TTable::Column<"id", int>;
+    using Col2 = TTable::Column<"age", int>;
+    auto col = Col{};
+    auto col2 = Col2{};
+    for (auto i = 0; i < ELEMENTS_PUSH_BACK; i++)
+    {
+        col.vec.push_back(1);
+        col2.vec.push_back(1);
+    }
+
+    for (auto _ : state)
+    {
+        TTable::add_two_columns<"new">(col, col2);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_AddTwoColumn);
+
 BENCHMARK_MAIN();
